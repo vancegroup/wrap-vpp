974fd694327a5a348356c15135ac508013bd9319
Ryan Pavlik <rpavlik@iastate.edu>
2010-02-19 11:07:00 -0600

    Initial work

---
+++ vpp-template.h
@@ -0,0 +1,37 @@
+/**
+	@file vpp.h
+	@brief C++ wrapper for Haption's VirtuoseAPI
+
+	@date
+	2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+#pragma once
+#ifndef _VPP_H
+#define _VPP_H
+
+// Standard includes
+
+
+// Library/third-party includes
+#include <VirtuoseAPI.h>
+
+// Internal Includes
+// none
+
+class Virtuose {
+
+/* BODY GOES HERE */
+
+	protected:
+	VirtContext m_vc;
+};
+
+#endif
