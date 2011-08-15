501de7cadf1070946c393601003e51fa958901a8
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-15 09:04:48 -0500

    define a macro for debug messages

--- vpp-boilerplate.h
+++ vpp-boilerplate.h
@@ -34,7 +34,10 @@
 #include <string>
 
 #ifdef VPP_VERBOSE
-#include <iostream>
+#	include <iostream>
+#	define VPP_VERBOSE_MESSAGE(_MSG) std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": " << _MSG << std::endl
+#else
+#	define VPP_VERBOSE_MESSAGE(_MSG)
 #endif
 
 class Virtuose {
@@ -58,9 +61,7 @@ class Virtuose {
 			_vc(virtOpen(_name.c_str())),
 			_weOpened(true)
 		{
-#ifdef VPP_VERBOSE
-			std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": Constructing a new Virtuose object, device named " << _name << ", VirtContext=" << _vc << std::endl;
-#endif
+			VPP_VERBOSE_MESSAGE("Constructing a new Virtuose object, device named " << _name << ", VirtContext=" << _vc);
 			if (_vc == NULL) {
 				_weOpened = false;
 				throw VirtuoseAPIError("Failed opening Virtuose " + _name + getErrorMessage());
@@ -73,9 +74,7 @@ class Virtuose {
 			_vc(vc),
 			_weOpened(false)
 		{
-#ifdef VPP_VERBOSE
-			std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": Constructing a Virtuose object from existing VirtContext " << _vc << std::endl;
-#endif
+			VPP_VERBOSE_MESSAGE("Constructing a Virtuose object from existing VirtContext " << _vc);
 		}
 
 		/// @brief Copy constructor
@@ -84,35 +83,27 @@ class Virtuose {
 			_vc(other._vc),
 			_weOpened(false)
 		{
-#ifdef VPP_VERBOSE
-			std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": Copy-constructing a Virtuose object from existing Virtuose object, named " << _name << ", VirtContext=" << _vc << std::endl;
-#endif
+			VPP_VERBOSE_MESSAGE("Copy-constructing a Virtuose object from existing Virtuose object, named " << _name << ", VirtContext=" << _vc);
 		}
 
 		/// @brief Assignment operator
 		Virtuose & operator=(Virtuose const& other) {
 			if (&other == this) {
 				// Self assignment - no-op
-#ifdef VPP_VERBOSE
-				std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": Self-assignment is a no-op" << std::endl;
-#endif
+				VPP_VERBOSE_MESSAGE("Self-assignment is a no-op");
 			} else {
 				// Actual assignment
 				int ret = 0;
 				if (_weOpened) {
 					// Close our existing one first.
-#ifdef VPP_VERBOSE
-					std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": In assignment operator, closing existing Virtuose device named " << _name << ", VirtContext=" << _vc << std::endl;
-#endif
+					VPP_VERBOSE_MESSAGE("In assignment operator, closing existing Virtuose device named " << _name << ", VirtContext=" << _vc);
 					ret = virtClose(_vc);
 				}
 				if (!checkForError(ret, __FILE__, __LINE__, __FUNCTION__)) {
 					_name = other._name;
 					_vc = other._vc;
 					_weOpened = false;
-	#ifdef VPP_VERBOSE
-					std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": Assignment operator has set ourselves to a device named " << _name << ", VirtContext=" << _vc << std::endl;
-	#endif
+					VPP_VERBOSE_MESSAGE("Assignment operator has set ourselves to a device named " << _name << ", VirtContext=" << _vc);
 				}
 			}
 			return *this;
@@ -123,15 +114,11 @@ class Virtuose {
 		*/
 		~Virtuose() {
 			if (_weOpened) {
-#ifdef VPP_VERBOSE
-			std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": In destructor for device named " << _name << ", VirtContext=" << _vc << ", closing because _weOpened flag is set" << std::endl;
-#endif
+				VPP_VERBOSE_MESSAGE("In destructor for device named " << _name << ", VirtContext=" << _vc << ", closing because _weOpened flag is set");
 				int ret = virtClose(_vc);
 				checkForError(ret, __FILE__, __LINE__, __FUNCTION__);
 			} else {
-#ifdef VPP_VERBOSE
-				std::cout << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")" << ": In destructor for device named " << _name << ", VirtContext=" << _vc << ", NOT closing because _weOpened flag is not set" << std::endl;
-#endif
+				VPP_VERBOSE_MESSAGE("In destructor for device named " << _name << ", VirtContext=" << _vc << ", NOT closing because _weOpened flag is not set");
 			}
 		}
 
@@ -152,9 +139,7 @@ func
 			if (returnValue == 0) {
 				return false; // no error
 			} else {
-#ifdef VPP_VERBOSE
-				std::cerr << file << ":" << line << " (" << func << ")" << "Got error from Virtuose: " << getErrorMessage() << std::endl;
-#endif
+				VPP_VERBOSE_MESSAGE("Got error from Virtuose: " << getErrorMessage());
 				return true; // error
 			}
 		}
