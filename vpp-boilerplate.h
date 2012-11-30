6b2cc9b9527e9abd828e111e3f7e89e99bc5029e
Ryan Pavlik <rpavlik@iastate.edu>
2012-11-30 12:02:01 -0600

    Update copyright.

--- vpp-boilerplate.h
+++ vpp-boilerplate.h
@@ -16,7 +16,7 @@
 	Human-Computer Interaction Graduate Program
 */
 
-//          Copyright Iowa State University 2010-2011.
+//          Copyright Iowa State University 2010-2012.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
